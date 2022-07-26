a0fd355f3b16bb8df12ee627cb3de260be671f0a
Nakius137 <40913643+Nakius137@users.noreply.github.com>
2022-07-26 14:51:36 +0200

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,8 @@
-import { AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js";
+import {
+  AuthenticationDetails,
+  CognitoUser,
+  CognitoUserAttribute,
+} from "amazon-cognito-identity-js";
 import UserPool from "../environment/UserPool";
 import React, { useState } from "react";
 import {
@@ -7,9 +11,12 @@ import {
   StyleduSubmitButton,
   StyledButtonsContainer,
 } from "../styles/StyledLogin";
+import useAppContext from "../hooks/useContext";
 
 export const Login = () => {
-  const handleLoginOnSubmit = () => {
+  const { setContextValue, contextValues } = useAppContext();
+
+  const handleOnSubmit = () => {
     const user = new CognitoUser({
       Username: login,
       Pool: UserPool,
@@ -21,39 +28,66 @@ export const Login = () => {
     });
 
     user.authenticateUser(authDetails, {
-      onSuccess: () => console.log("Sukces"),
+      onSuccess: () => {
+        console.log("Sukces");
+        setContextValue({ ...contextValues, isAuth: true });
+      },
       onFailure: (error) => console.log(`Fail - ${error}`),
       newPasswordRequired: () => console.log("newpaswddreq"),
     });
   };
 
-  const handleRegisterOnSubmit = () => {
-    console.log("halo");
-    //@ts-ignore
-    UserPool.signUp(login, password, [], null, (err, data) => {
-      if (err) console.error(err);
-      console.log(data);
-    });
-    console.log("halo");
+  const setCognitoUserAttribute = (
+    attributeKey: string,
+    attributeValue: string
+  ) => {
+    let data = {
+      Name: attributeKey,
+      Value: attributeValue,
+    };
+
+    return new CognitoUserAttribute(data);
   };
+
+  const handleRegisterOnSubmit = () => {
+    let email = login;
+    let attributeList = [];
+
+    attributeList.push(setCognitoUserAttribute("email", email));
+
+    UserPool.signUp(
+      email,
+      password,
+      attributeList,
+      //@ts-ignore
+      null,
+      (err, data) => {
+        if (err) console.error(err);
+        alert("Udało się zarejestrować, możesz przejść do logowania");
+      }
+    );
+  };
+
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
 
   return (
     <StyledComponent>
       <StyledInput
+        placeholder="Wpisz email"
         onChange={(e) => {
           setLogin(e.target.value);
         }}
       ></StyledInput>
       <StyledInput
+        placeholder="Wpisz hasło"
         type="password"
         onChange={(e) => {
           setPassword(e.target.value);
         }}
       ></StyledInput>
       <StyledButtonsContainer>
-        <StyleduSubmitButton onClick={handleLoginOnSubmit}>
+        <StyleduSubmitButton onClick={handleOnSubmit}>
           Zaloguj się
         </StyleduSubmitButton>
         <StyleduSubmitButton onClick={handleRegisterOnSubmit}>
@@ -63,3 +97,4 @@ export const Login = () => {
     </StyledComponent>
   );
 };
+
